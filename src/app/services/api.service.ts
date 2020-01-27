675c049daf03e2e3af1b2334ba6205ad45e8b736
marcellevargas <37669732+marcellevargas@users.noreply.github.com>
2020-01-27 15:11:53 -0300

    api.service.ts

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+  
+  get(){ 
+    
+  }
 }
